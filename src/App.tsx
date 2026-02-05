4107198e84814f7d70c18b05581ef19a2af3ecb3
CdeB-img <debourguignoncharles@gmail.com>
2026-02-05 18:22:38 +0100

    tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
+
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
@@ -12,7 +13,9 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-// Composant pour scroll to top lors de la navigation
+/**
+ * Scroll to top on route change
+ */
 function ScrollToTop() {
   const { pathname } = useLocation();
 
@@ -28,17 +31,19 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      <HashRouter>
         <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projets" element={<Projects />} />
           <Route path="/projet/:id" element={<ProjectDetail />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
