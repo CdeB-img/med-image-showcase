4d56e05c296e5ce3b9fda463ecc05c1c4c09a3e7
CdeB-img <debourguignoncharles@gmail.com>
2026-02-06 02:38:36 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/NOXIA/" : "/",
-  server: {
-    port: 8080,
-  },
+export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
+
+
