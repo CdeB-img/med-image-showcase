458e7054c8874031d49a9c3decddfbf07bfdb347
CdeB-img <debourguignoncharles@gmail.com>
2026-02-11 17:05:07 +0100

    b

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from " react-dom/client " ;     
-import App from " ./App.tsx " ;   
-import " ./index.css " ; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 
-créerRoot ( document.getElementById ( " root " ) ! ) . render ( < App / > ) ;​ 
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
