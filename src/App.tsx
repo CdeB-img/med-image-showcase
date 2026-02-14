16f4aaf2aa7ceafd8037aa960855311d9f2a8b55
CdeB-img <debourguignoncharles&gmail.com>
2026-02-14 02:29:27 +0100

    aze

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,6 @@ import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import { track } from "@vercel/analytics/react";
 
 
 
@@ -46,17 +45,7 @@ function ScrollToTop() {
   return null;
 }
 
-function RouteTracker() {
-  const location = useLocation();
 
-  useEffect(() => {
-    track("pageview", {
-      path: location.pathname + location.search,
-    });
-  }, [location.pathname, location.search]);
-
-  return null;
-}
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -64,7 +53,6 @@ const App = () => (
       <Sonner />
 
       <BrowserRouter basename="/">
-        <RouteTracker />
         <ScrollToTop />
 
         {/* 🔹 NAVBAR GLOBALE */}
